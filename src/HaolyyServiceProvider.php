4f46f1571fcad31555e3a720f30dd9306638ef06
dongzhuo <dongzhuo@git.haolyy.com>
2016-03-10 09:55:17 +0800

    最新修改

--- src/HaolyyServiceProvider.php
+++ src/HaolyyServiceProvider.php
@@ -21,6 +21,7 @@ class ToolsServiceProvider extends ServiceProvider {
         $this->registerCheckMobileValidator();
         $this->registerCheckIdCardValidator();     
         $this->registerCheckNameValidator();
+        $this->registerCheckPasswordValidator();
     }
 
     /**
@@ -73,7 +74,7 @@ class ToolsServiceProvider extends ServiceProvider {
      */
     protected function registerCheckMobileValidator()
     {
-        return Validator::extend('mobile', function($attribute, $value, $parameters, $validator) {
+        return Validator::extend('haolyyMobile', function($attribute, $value, $parameters, $validator) {
             //判断长度11位，全数字
             if (!is_numeric($value) || mb_strlen($value) != 11) {
                 return false;
@@ -92,7 +93,7 @@ class ToolsServiceProvider extends ServiceProvider {
      */
     protected function registerCheckIdCardValidator()
     {
-        return Validator::extend('idcard', function($attribute, $value, $parameters, $validator) {
+        return Validator::extend('haolyyIdCard', function($attribute, $value, $parameters, $validator) {
             $vCity = [
                 '11', '12', '13', '14', '15', '21', '22',
                 '23', '31', '32', '33', '34', '35', '36',
@@ -140,15 +141,37 @@ class ToolsServiceProvider extends ServiceProvider {
         });
     }
 
+
     /**
      * [registerCheckMobileValidator 验证姓名]
      * @return [type] [description]
      */
     protected function registerCheckNameValidator()
     {
-        return Validator::extend('name', function($attribute, $value, $parameters, $validator) {
+        return Validator::extend('haolyyName', function($attribute, $value, $parameters, $validator) {
             $patternString = "/^[\x{4e00}-\x{9fa5}·]+$/u";
             return preg_match($patternString, $value);
         });
+    }
+
+
+
+    /**
+     * [checkPassword 验证密码是否合法]
+     * @param  [string] $password [密码]
+     * @return [bool]           [验证结果]
+     */
+    protected function registerCheckPasswordValidator()
+    {
+        return Validator::extend('haolyyPassword', function($attribute, $value, $parameters, $validator) {
+            $plen = mb_strlen($value);
+            //必须英文加数字，6到12位
+            if (preg_match("/(?!^(\d+|[a-zA-Z]+|[w~!@#$%._]+)$)^[\w~!@#$%._?]+$/i", $value) && $plen >= 6 && $plen <= 12) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
     }
 }
